d99385fcf23d476687f49875873b5c6c30a58244
Paul Jones <shmiko@gmail.com>
2015-03-22 20:10:52 +1100

    MEANjsProject040

--- config/env/production.js
+++ config/env/production.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // MongoHQ URL can be set here, or on the webhost.
-process.env.MONGOHQ_URL = 'mongodb://tripstomp:waxnepke@ds031641.mongolab.com:31641/calmapit';
+//process.env.MONGOHQ_URL = 'mongodb://tripstomp:waxnepke@ds031641.mongolab.com:31641/calmapit';
 //db: 'mongodb://tripstomp:waxnepke@ds031641.mongolab.com:31641/calmapit',
     
 module.exports = {
-    db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    db: 'mongodb://tripstomp:waxnepke@ds031641.mongolab.com:31641/calmapit',
     facebook: {
         clientID: process.env.FACEBOOK_ID || 'APP_ID',
         clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
